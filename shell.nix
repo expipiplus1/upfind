cb451254f5b112f839aa36e5b6fd83b60cf9b9ae
Joe Hermaszewski <git@monoid.al>
2019-10-12 20:15:18 +0800

    Improve nix expressions

--- shell.nix
+++ shell.nix
@@ -1,3 +1,3 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { }, compiler ? null, hoogle ? true }:
 
-(import ./default.nix { inherit pkgs; }).env
+(import ./default.nix { inherit pkgs compiler hoogle; }).env
